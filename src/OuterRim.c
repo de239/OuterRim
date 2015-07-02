6387f5090a19f1331e119d96b7d8d0733ca5f07b
David Evans <d.f.evans@derby.ac.uk>
2015-07-02 15:48:51 +0100

    Add tap-based second hand.

--- src/OuterRim.c
+++ src/OuterRim.c
@@ -1,38 +1,36 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
-static Layer *window_layer;
+static Layer *face_layer;
+
+static bool display_second_hand;
+
+static GPoint centre;
+static int32_t circle_radius;
+static int32_t second_hand_radius;
+static int32_t inner_radius;
+static int32_t inner_dot;
+static int32_t outer_hand_length;
+static int32_t inner_hand_length;
 
 #define FACE_BACKGROUND_COLOUR GColorBlack
 #define CIRCLE_COLOUR GColorWhite
+#define SECOND_HAND_CIRCLE_COLOUR GColorRed
 #define OUTER_HAND_COLOUR GColorWhite
+#define SECOND_HAND_COLOUR GColorGreen
 #define INNER_CIRCLE_COLOUR GColorBrilliantRose
 #define INNER_HAND_COLOUR GColorBrilliantRose
 #define INNER_DOT_COLOUR GColorDarkGray
 
-/*
-void tap(AccelAxisType axis, int32_t direction) {
-  text_layer_set_text(text_layer, "Tap");
-}
-*/
+#define SECOND_HAND_DURATION 2
 
-static void window_update_proc(Layer *l, GContext *ctx) {
+static void face_update_proc(Layer *l, GContext *ctx) {
 	time_t now = time(NULL);
 	struct tm t = *localtime(&now);
-
-	GRect f = layer_get_bounds(l);
-	grect_standardize(&f);
-	GPoint centre = grect_center_point(&f);
-	int32_t circle_radius = (f.size.w / 2) - 2;
-	int32_t inner_radius = circle_radius / 5;
-	int32_t inner_dot = inner_radius / 3;
-
 	GPoint outer_hand_start;
 	GPoint outer_hand_end;
-	int32_t outer_hand_length = circle_radius / 2;
 	GPoint inner_hand_end;
-	int32_t inner_hand_length = circle_radius / 2 + inner_radius;
+	GPoint second_hand_end;
 
 	int32_t angle = TRIG_MAX_ANGLE * t.tm_min / 60;
 	outer_hand_start.y = (-cos_lookup(angle) * circle_radius / TRIG_MAX_RATIO) + centre.y;
@@ -50,11 +48,22 @@ static void window_update_proc(Layer *l, GContext *ctx) {
 	graphics_fill_rect(ctx, layer_get_bounds(l), 0, GCornerNone);
 
 	graphics_context_set_stroke_width(ctx, 1);
-
 	graphics_context_set_stroke_color(ctx, CIRCLE_COLOUR);
 	graphics_draw_circle(ctx, centre, circle_radius);
 
 	graphics_context_set_stroke_width(ctx, 3);
+
+	if(display_second_hand) {
+		graphics_context_set_fill_color(ctx, SECOND_HAND_CIRCLE_COLOUR);
+		graphics_fill_circle(ctx, centre, second_hand_radius);
+
+		angle = TRIG_MAX_ANGLE * t.tm_sec / 60;
+		second_hand_end.y = (-cos_lookup(angle) * second_hand_radius / TRIG_MAX_RATIO) + centre.y;
+		second_hand_end.x = (sin_lookup(angle) * second_hand_radius / TRIG_MAX_RATIO) + centre.x;
+
+		graphics_context_set_stroke_color(ctx, SECOND_HAND_COLOUR);
+		graphics_draw_line(ctx, centre, second_hand_end);
+	}
 
 	graphics_context_set_stroke_color(ctx, OUTER_HAND_COLOUR);
 	graphics_draw_line(ctx, outer_hand_start, outer_hand_end);
@@ -70,22 +79,59 @@ static void window_update_proc(Layer *l, GContext *ctx) {
 }
 
 static void window_load(Window *window) {
-  window_layer = window_get_root_layer(window);
+	Layer *window_layer = window_get_root_layer(window);
+	face_layer = layer_create(layer_get_frame(window_layer));
+	layer_add_child(window_layer, face_layer);
 
-  layer_set_update_proc(window_layer, window_update_proc);
+	GRect f = layer_get_bounds(face_layer);
+	grect_standardize(&f);
+	centre = grect_center_point(&f);
+	circle_radius = (f.size.w / 2) - 2;
+	second_hand_radius = circle_radius * 6 / 10;
+	inner_radius = circle_radius / 5;
+	inner_dot = inner_radius / 3;
+
+	outer_hand_length = circle_radius / 2;
+	inner_hand_length = circle_radius / 2 + inner_radius;
+
+	display_second_hand = false;
+
+	layer_set_update_proc(face_layer, face_update_proc);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+	layer_destroy(face_layer);
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-	layer_mark_dirty(window_layer);
+	layer_mark_dirty(face_layer);
+}
+
+static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
+	static uint8_t seconds_displayed = 0;
+
+	if(seconds_displayed++ > SECOND_HAND_DURATION) {
+		display_second_hand = false;
+		seconds_displayed = 0;
+
+		tick_timer_service_unsubscribe();
+		tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	}
+
+	layer_mark_dirty(face_layer);
+}
+
+static void tap(AccelAxisType axis, int32_t direction) {
+	display_second_hand = true;
+	layer_mark_dirty(face_layer);
+
+	tick_timer_service_unsubscribe();
+	tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 }
 
 static void init(void) {
   window = window_create();
-  //accel_tap_service_subscribe(tap);
+  accel_tap_service_subscribe(tap);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
